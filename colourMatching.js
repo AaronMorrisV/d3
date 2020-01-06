f42989545827307859cdb1129ee99d23135581b3
xlzf27 <xlzf27@durham.ac.uk>
2020-01-06 03:47:51 +0000

    com

--- colourMatching.js
+++ colourMatching.js
@@ -1,71 +1,112 @@
 /*colour matching*/
-    //base sepia HSL colour space
-    var baseH = 38;
-    var baseS = 21.4;
-    var baseL = 54.1;
 
-    var input1 = document.getElementById("col1");
-    var input2 = document.getElementById("col2");
+//input field colour matching
+updateInputColour(document.getElementById('col1'));
+updateInputColour(document.getElementById('col2'));
+function updateInputColour(_elem){
+    //Ensure correct length of hex code (6 digits)
+    while(_elem.value.length < 6){
+        _elem.value += "f";
+    }
+    if(_elem.value.length > 6){
+        _elem.value = _elem.value.slice(0,6);
+    }
+    //Change background colour of input field
+    _elem.style.backgroundColor = "#"+_elem.value;
+
+    //Change colour of text (black text on light backgrounds, white text on dark backgrounds)
+    if (parseInt(_elem.value.slice(0,2),16)*0.299 + parseInt(_elem.value.slice(2,4),16)*0.587 + parseInt(_elem.value.slice(4,6),16)*0.114 > 186){
+        _elem.style.color = "#000000";
+    } 
+    else{
+        _elem.style.color = "#ffffff";
+    }
+}
+
+//base sepia HSL colour space
+var baseH = 38;
+var baseS = 21.4;
+var baseL = 54.1;
+
+var input1 = document.getElementById("col1");
+var input2 = document.getElementById("col2");
+
+input1.addEventListener('change', function (evt) {
+    sakuraColourChange(this.value,input2.value)
+    updateInputColour(this);
     backgroundChange();
-    input1.addEventListener('change', function (evt) {
-        console.log(input1.value);
-        backgroundChange();
-        sakuraColourChange(this.value,input2.value)
-    });
-    input2.addEventListener('change', function (evt) {
-        backgroundChange();
-        sakuraColourChange(input1.value,this.value)
-    });
+    updateAxisColour();
+});
+input2.addEventListener('change', function (evt) {
+    sakuraColourChange(input1.value,this.value)
+    updateInputColour(this);
+    backgroundChange();
+    updateAxisColour();
+});
 
-    function backgroundChange(){
-        document.body.style.background = "linear-gradient(0deg, #"+input1.value+" -10%, #"+input2.value+" 15%, #ffffff 50%)";
-        console.log(document.body.style.background);
-    }
-    function sakuraColourChange(col1,col2){
-        _hsl = hexToHSL(lerpColor("#"+col1,"#"+col2,0.5));
-        var newH = _hsl.h - baseH;
-        var newS = 100 + (_hsl.l-baseS);
-        var newL = 100 + (_hsl.l-baseL)
-        document.getElementById('snow-bg').style.filter = "brightness(50%) sepia(1) hue-rotate("+ newH +"deg) saturate("+ newS +"%) brightness("+ newL +"%)";
-    }
-    
+backgroundChange();
+function backgroundChange(){
+    document.body.style.background = "linear-gradient(0deg, #"+input2.value+" -10%, #"+input1.value+" 15%, #ffffff 50%)";
+}
 
-    //hexToHsl function by https://gist.github.com/xenozauros/f6e185c8de2a04cdfecf
-    function hexToHSL(hex) {
-        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-            r = parseInt(result[1], 16);
-            g = parseInt(result[2], 16);
-            b = parseInt(result[3], 16);
-            r /= 255, g /= 255, b /= 255;
-            var max = Math.max(r, g, b), min = Math.min(r, g, b);
-            var h, s, l = (max + min) / 2;
-            if(max == min){
-            h = s = 0; // achromatic
-            }else{
-            var d = max - min;
-            s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-            switch(max){
-                case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-                case g: h = (b - r) / d + 2; break;
-                case b: h = (r - g) / d + 4; break;
-            }
-            h /= 6;
-            }
-        var HSL = new Object();
-        HSL['h']=h*360;
-        HSL['s']=s*100;
-        HSL['l']=l*100;
-        return HSL;
-    }
+function sakuraColourChange(col1,col2){
+    _hsl = hexToHSL(lerpColor("#"+col1,"#"+col2,0.5));
+    var newH = _hsl.h - baseH;
+    var newS = 100 + (_hsl.l-baseS);
+    var newL = 100 + (_hsl.l-baseL)
+    document.getElementById('snow-bg').style.filter = "brightness(50%) sepia(1) hue-rotate("+ newH +"deg) saturate("+ newS +"%) brightness("+ newL +"%)";
+}
 
-    //Lerp Color function by Rosszurowski https://gist.github.com/rosszurowski/67f04465c424a9bc0dae
-    const lerpColor = (a, b, amount) =>{ 
-        var ah = parseInt(a.replace(/#/g, ''), 16),
-            ar = ah >> 16, ag = ah >> 8 & 0xff, ab = ah & 0xff,
-            bh = parseInt(b.replace(/#/g, ''), 16),
-            br = bh >> 16, bg = bh >> 8 & 0xff, bb = bh & 0xff,
-            rr = ar + amount * (br - ar),
-            rg = ag + amount * (bg - ag),
-            rb = ab + amount * (bb - ab);
-        return '#' + ((1 << 24) + (rr << 16) + (rg << 8) + rb | 0).toString(16).slice(1);
+//hexToHsl function by https://gist.github.com/xenozauros/f6e185c8de2a04cdfecf
+function hexToHSL(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        r = parseInt(result[1], 16);
+        g = parseInt(result[2], 16);
+        b = parseInt(result[3], 16);
+        r /= 255, g /= 255, b /= 255;
+        var max = Math.max(r, g, b), min = Math.min(r, g, b);
+        var h, s, l = (max + min) / 2;
+        if(max == min){
+        h = s = 0; // achromatic
+        }else{
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max){
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+        }
+    var HSL = new Object();
+    HSL['h']=h*360;
+    HSL['s']=s*100;
+    HSL['l']=l*100;
+    return HSL;
+}
+
+//Lerp Color function by Rosszurowski https://gist.github.com/rosszurowski/67f04465c424a9bc0dae
+const lerpColor = (a, b, amount) =>{ 
+    var ah = parseInt(a.replace(/#/g, ''), 16),
+        ar = ah >> 16, ag = ah >> 8 & 0xff, ab = ah & 0xff,
+        bh = parseInt(b.replace(/#/g, ''), 16),
+        br = bh >> 16, bg = bh >> 8 & 0xff, bb = bh & 0xff,
+        rr = ar + amount * (br - ar),
+        rg = ag + amount * (bg - ag),
+        rb = ab + amount * (bb - ab);
+    return '#' + ((1 << 24) + (rr << 16) + (rg << 8) + rb | 0).toString(16).slice(1);
+}
+
+
+function updateAxisColour(){
+    var newColour = lerpColor(col1.value,col2.value,0.5);
+    var lines = document.getElementsByTagName("line");
+    for(var i = 0; i < lines.length; i++){
+       lines[i].style.stroke = newColour;
     }
+    var texts = document.getElementsByTagName("text");
+    console.log(texts, texts.length);
+    for(var i = 0; i < texts.length; i++){
+        texts[i].style.fill = newColour;
+     }
+}
--- simpled3.js
+++ simpled3.js
@@ -1,168 +1,253 @@
 class Graph{
+    //Constructor taking the width and height (in pixels) as well as the data from which the graph will be made
     constructor(width,height,data){
-        this.height = height;
-        this.width = width;
-        this.data = data;
+        this.height = (typeof height !== 'undefined') ? height : 500;
+        this.width = (typeof height !== 'undefined') ? width : 600;
+        //this.height = height;
+        //this.width = width;
+        this.data = (typeof height !== 'undefined') ? data : "https://raw.githubusercontent.com/AaronMorrisV/d3/master/data1.csv";
+        //this.data = data;
     }
     
-    graphInit(container){
-        var margin = {top: 20, right: 30, bottom: 0, left: 10},
+    set graphTitle(_title){
+        var title = _title;
+        document.getElementById("title").innerHTML = title;
+    }
+
+    set colour1(_col){
+        var col = this.cleanseInput(_col);
+        col1.value = col;
+        updateInputColour(col1);
+        this.updateGraphColours(col, col2.value);
+    }
+    get colour1(){
+       return col1.value;
+    }
+
+    set colour2(_col){
+        var col = this.cleanseInput(_col);
+        col2.value = _col;
+        updateInputColour(col2);
+        this.updateGraphColours(col1.value,col);
+    }
+    get colour2(){
+        return col2.value;
+    }
+
+    //Ensure that the input is a valid hex colour (6 digits)
+    cleanseInput(val){
+        while(val.length < 6){
+            val += "f";
+        }
+        if(val.length > 6){
+            val = val.slice(0,6);
+        }
+        return val;
+    }
+
+    set scale(_scale){
+        d3.select("#graph")
+            .style("transform","translate(-50%,-50%) scale("+_scale+")")
+        document.getElementById("scale").value = _scale;
+    }
+    get scale(){
+        return document.getElementById("scale").value;
+    }
+
+    updateGraphColours(_col1, _col2){
+        var _this = this;
+        d3.csv(this.data, function(data) {
+            var keys = data.columns.slice(1)
+
+            var color = d3.scaleOrdinal()
+                .domain(keys)
+                .range(_this.genColorPal(_col1,_col2, keys.length));
+
+            d3
+                .selectAll("path")
+                .transition()
+                .duration(1000)
+                .style("fill", function(d) { return color(d.key); })
+        })
+    }
+
+    //Generate a colour palette. I.e. if there are 5 different sections in the graph, linearly interpolate between two colour values a and b in 5 equal intervals
+    genColorPal(a,b,num){
+        var i = 0;
+        var arry = [];
+        while(i < num){
+            //Lerp Color function by Rosszurowski https://gist.github.com/rosszurowski/67f04465c424a9bc0dae
+            //lerpColor() is reference to colourMatching.js script
+            arry.push(lerpColor(a,b,i/(num-1)))
+            i+=1;
+        }
+        return arry;
+    }
+
+    graphInit(){
+        //Apply margins to the graph and adjust the height and width of the graph accordingly
+        var margin = {top: 50, right: 50, bottom: 50, left: 50},
             width = this.width - margin.left - margin.right,
             height = this.height - margin.top - margin.bottom;
-        d3.select('body').selectAll('.dimension')
-                //.attr('class', 'jscolor')
-                //.attr('value', "0000ff")
-                .on('change',function () {
-                    var scale1 = d3.select('#scale1').property('value');
-                    var scale2 = d3.select('#scale2').property('value');
-                    d3.select("#graph1")
-                        .style("transform","scale("+scale1+")")
-                        //.attr("style", "transform:scale("+scale+") translate(-50%,-50%); transform-origin: top left; transition:0.3s; position:absolute;top:50%;left:50%;")
-                    d3.select("#graph2")
-                        .style("transform","scale("+scale2+")")
-                        //.attr("style", "transform:scale("+scale+") translate(-50%,-50%); transform-origin: top left; transition:0.3s; position:absolute;top:50%;left:50%;")
-                    
-                })
         
-
-        var svg = d3.select(container)
+        //append an svg (with appropriate width and height) to the container with a g as child
+        var svg = d3.select("#graph")
             .append("svg")
                 .attr("width", width + margin.left + margin.right)
                 .attr("height", height + margin.top + margin.bottom)
             .append("g")
                 .attr("transform",
                     "translate(" + margin.left + "," + margin.top + ")");
-
-
-        const genColorPal = (a, b, num) =>{
-            var i = 0;
-            var arry = [];
-            while(i < num){
-                arry.push(lerpColor(a,b,i/(num-1)))
-                i+=1;
-            }
-            return arry;
-        }
         
-        //Lerp Color function by Rosszurowski https://gist.github.com/rosszurowski/67f04465c424a9bc0dae
-        const lerpColor = (a, b, amount) =>{ 
-            var ah = parseInt(a.replace(/#/g, ''), 16),
-                ar = ah >> 16, ag = ah >> 8 & 0xff, ab = ah & 0xff,
-                bh = parseInt(b.replace(/#/g, ''), 16),
-                br = bh >> 16, bg = bh >> 8 & 0xff, bb = bh & 0xff,
-                rr = ar + amount * (br - ar),
-                rg = ag + amount * (bg - ag),
-                rb = ab + amount * (bb - ab);
-            return '#' + ((1 << 24) + (rr << 16) + (rg << 8) + rb | 0).toString(16).slice(1);
-        }
-        
+        var _this = this; //Need to maintain reference to "this" withing d3.csv() function.
+
         // Parse the Data
         d3.csv(this.data, function(data) {
-            // List of groups = header of the csv files
-            var keys = data.columns.slice(1)
-
-            // Add X axis
-            var x = d3.scaleLinear()
-                .domain(d3.extent(data, function(d) { return d.year; }))
+            //The keys of the data are the first column in the data, so we slice the first column of the data to obtain the keys
+            var keys = data.columns.slice(1);
+            
+            //Add X axis
+            /*var x = d3.scaleLinear()
+                .domain(d3.extent(data, function(d) { console.log(d.year);return d.year;  }))
                 .range([ 0, width ]);
-            svg.append("g")
-                .attr("transform", "translate(0," + height*0.8 + ")")
-                .call(d3.axisBottom(x).tickSize(-height*.7).tickValues([1900, 1925, 1975, 2000]))
-                .select(".domain").remove()
-            // Customization
+            console.log(d3.extent(data, function(d) { return d.year;  }));*/
+            var dates = d3.extent(data, function(d) { return d.year;  });
+
+            var minDate = new Date(dates[0]),
+                maxDate = new Date(dates[1]);
+            //var x = d3.time.scale().domain([minDate, maxDate]).range([0, w]);
+            var x = d3.scaleTime()
+                .domain([minDate, maxDate])
+                .range([ 0, width ]);
+
+            /*var max = 0;
+            var allData;
+            console.log(data.columns);
+            for (var i = 0; i < keys.length; i++) {
+                max = Math.max(max, d3.max(data, function(d) { return d.hey; } ));
+                console.log(max);
+                //Do something
+            }*/
+            var x_axis = d3.axisBottom()
+                   .scale(x);
+                svg.append("g")
+                    .attr("class", "xAxis")
+                   .call(x_axis);
+
+            //Customization of tick values
             svg.selectAll(".tick line").attr("stroke", "#b8b8b8")
 
-            // Add X axis label:
+            //Add X axis label:
             svg.append("text")
                 .attr("text-anchor", "end")
-                .attr("x", width)
-                .attr("y", height-30 )
-                .text("Time (year)");
+                //.attr("x", 0)
+                .attr("y", -7)
+                .text("Time")
+                .attr("class", "time");
 
-            // Add Y axis
+            //Add Y axis
             var y = d3.scaleLinear()
-                .domain([-100000, 100000])
+                .domain([-30000000, 30000000])
                 .range([ height, 0 ]);
 
+            //Initialise the graph to use the values of the colour selectors
             var col1 = d3.select('#col1').property('value');
             var col2 = d3.select('#col2').property('value');
 
+            //Create an ordinal scale variable with the domain of the keys and a range equal to the colour palette created by linearly interpolating between the two colours keys.length number of times
             var color = d3.scaleOrdinal()
                 .domain(keys)
-                .range(genColorPal(col1,col2, keys.length)); //d3.schemeDark2
+                .range(_this.genColorPal(col1,col2, keys.length));
 
-
-            //stack the data?
+            //Stack the data to create streamgraph
             var stackedData = d3.stack()
                 .offset(d3.stackOffsetSilhouette)
                 .keys(keys)
                 (data)
 
-            // create a tooltip
-            var Tooltip = svg
-                .append("text")
-                .attr("x", 0)
-                .attr("y", 0)
-                .style("opacity", 0)
-                .style("font-size", 17)
+            //Three functions that change the tooltip when user hover / move / leave a cell
 
-            // Three function that change the tooltip when user hover / move / leave a cell
+            //On mouseover of a graph area....
             var mouseover = function(d) {
+                //Fade out all graph areas
                 Tooltip.style("opacity", 1)
                 d3.selectAll(".myArea")
                 .style("opacity", .2)
                 
+                //Fade in and scale up the graph area which is currently being hovered over
                 d3.select(this)
                 .style("opacity", 1)
-                .style("transform", "scale(1.2)")
             }
+            //On mousemove...
             var mousemove = function(d,i) {
-                var grp = keys[i]
-                Tooltip.text(grp)
+                //Set the text of the tooltip equal to the value of the key which is currently being hovered over
+                var grp = keys[i];
+                //console.log(grp);
+                //Tooltip.text(grp)
+                document.getElementById('title').innerHTML = grp;
             }
+            //On mouse leave...
             var mouseleave = function(d) {
+                //Fade out tooltip
                 Tooltip.style("opacity", 0)
-                d3.selectAll(".myArea").style("opacity", 1).style("stroke", "none").style("transform", "scale(1)")
+                //Fade in all graph areas
+                d3.selectAll(".myArea").style("opacity", 1).style("transform", "scale(1)")
+                //document.getElementById('fuji').innerHTML = 
             }
 
-            // Area generator
+            //Area generator
             var area = d3.area()
-                .x(function(d) { return x(d.data.year); })
-                .y0(function(d) { return y(d[0]); })
-                .y1(function(d) { return y(d[1]); })
+                .x(function(d) { return x(new Date(d.data.year)); })
+                .y0(function(d) {  return y(d[0]); })
+                .y1(function(d) {  return y(d[1]); })
 
-            // Show the areas
+
+            //Show the areas
             svg
                 .selectAll("mylayers")
                 .data(stackedData)
                 .enter()
                 .append("path")
                 .attr("class", "myArea")
+                //Style the areas
                 .style("transition", "0.5s")
                 .style("transform-origin", "center center")
-                .style("fill", function(d) { 
-                    return color(d.key); 
-                })
+                .style("fill", function(d) { return color(d.key); })
+                //Events
                 .attr("d", area)
                 .on("mouseover", mouseover)
                 .on("mousemove", mousemove)
                 .on("mouseleave", mouseleave)
 
-            d3.select('body').selectAll('.jscolor')
-                //.attr('class', 'jscolor')
-                //.attr('value', "0000ff")
+
+            //Handle the interactive colour selection
+            d3.select('body').selectAll('.colourInput')
+                //When either color selector changes...
                 .on('change',function () {
                     var col1 = d3.select('#col1').property('value');
                     var col2 = d3.select('#col2').property('value');
+                    //Create an ordinal scale variable with the domain of the keys and a range equal to the colour palette created by linearly interpolating between the two colours keys.length number of times
                     var color = d3.scaleOrdinal()
                             .domain(keys)
-                            .range(genColorPal(col2,col1, keys.length)); //d3.schemeDark2
+                            .range(_this.genColorPal(col1,col2, keys.length));
+                    //Update colours of the areas
                     d3
-                        .selectAll("path")
+                        .selectAll(".myArea")
                         .transition()
                         .duration(1000)
                         .style("fill", function(d) { return color(d.key); })
+                        
                 })
+            
+            //Handle the interactive scaling selection
+            d3.select('body').selectAll('.dimension')
+                .on('change',function () {
+                    var scale = d3.select('#scale').property('value');
+                    //Set the transform:scale() styling property of the graph equal to the value of the scale input
+                    d3.select("#graph")
+                        .style("transform","translate(-50%,-50%) scale("+scale+")")
+                })
+            
         })
     }
 }
